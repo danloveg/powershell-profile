1bab5199d763ca1fadeefa1c8808dc16c7e5b676
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-07-20 10:43:33 -0500

    Create Alias module

---
+++ Modules/Alias/Alias.psm1
@@ -0,0 +1,8 @@
+# Common Aliases
+# Author: Daniel Lovegrove
+
+Set-Alias vim "C:/Program Files (x86)/Vim/vim81/vim.exe"
+Set-Alias notepad++ "C:/Program Files (x86)/Notepad++/notepad++.exe"
+Set-Alias 7z "C:/Program Files/7-Zip/7z.exe"
+
+Export-ModuleMember -Alias "vim", "notepad++", "7z"
