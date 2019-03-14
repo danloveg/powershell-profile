26593176516420adeba9f9285a08807b36f861ff
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-03-13 20:35:41 -0500

    Fix inconsistent slashes

--- Modules/Alias/Alias.psm1
+++ Modules/Alias/Alias.psm1
@@ -1,9 +1,9 @@
 # Common Aliases
 # Author: Daniel Lovegrove
 
-Set-Alias vim "C:/Program Files (x86)/Vim/vim81/vim.exe"
-Set-Alias notepad++ "C:/Program Files (x86)/Notepad++/notepad++.exe"
-Set-Alias 7z "C:/Program Files/7-Zip/7z.exe"
+Set-Alias vim "C:\Program Files (x86)\Vim\vim81\vim.exe"
+Set-Alias notepad++ "C:\Program Files (x86)\Notepad++\notepad++.exe"
+Set-Alias 7z "C:\Program Files\7-Zip\7z.exe"
 Set-Alias Typora "C:\Program Files\Typora\Typora.exe"
 
 Export-ModuleMember -Alias @(
@@ -12,4 +12,3 @@ Export-ModuleMember -Alias @(
     "7z",
     "Typora"
 )
-
