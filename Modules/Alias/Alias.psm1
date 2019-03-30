fd8c8f4fbc78b389f5d6a72c71349ec1d96450a6
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-03-30 14:40:54 -0500

    Add alias to new Set-LocationLink function

--- Modules/Alias/Alias.psm1
+++ Modules/Alias/Alias.psm1
@@ -5,10 +5,12 @@ Set-Alias vim "C:\Program Files (x86)\Vim\vim81\vim.exe"
 Set-Alias notepad++ "C:\Program Files (x86)\Notepad++\notepad++.exe"
 Set-Alias 7z "C:\Program Files\7-Zip\7z.exe"
 Set-Alias Typora "C:\Program Files\Typora\Typora.exe"
+Set-Alias Follow-Link Set-LocationLink
 
 Export-ModuleMember -Alias @(
     "vim",
     "notepad++",
     "7z",
-    "Typora"
+    "Typora",
+    "Follow-Link"
 )
