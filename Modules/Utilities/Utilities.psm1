2b7c9c2255fdb6fc5cd526ca0869fcc19bf6b7c9
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-10-30 09:52:35 -0500

    Remove Functions I never use

--- Modules/Utilities/Utilities.psm1
+++ Modules/Utilities/Utilities.psm1
@@ -3,14 +3,6 @@
 
 Import-Module Alias
 
-Function Edit-Profile {
-    code "C:\Users\dlove\Google Drive\PowerShell\"
-}
-
-Function Edit-Vimrc {
-    vim $HOME\_vimrc
-}
-
 Function Get-MainProcesses {
     Get-Process | Where-Object {$_.MainWindowTitle} | Format-Table Id, Name, MainWindowTitle -autosize
 }
@@ -60,28 +52,10 @@ Function Test-Administrator {
     return 0 -eq (id -u)
 }
 
-Function Repair-Emulators {
-    # Removing .img files for Android emulators seems to make them work again
-    Get-ChildItem "C:\Users\dlove\.android\avd\*\*.img" | Remove-Item
-}
-
-Function Repair-Nexus7 {
-    Remove-Item "C:\Users\dlove\.android\avd\Nexus_7_API_25.avd\*.img"
-}
-
-Function Repair-Pixel {
-    Remove-Item "C:\Users\dlove\.android\avd\Pixel_API_26_V3.avd\*.img"
-}
-
 Export-ModuleMember -Function @(
-    "Edit-Profile",
-    "Edit-Vimrc",
     "Get-MainProcesses",
     "Get-DiskUsage",
     "Touch",
     "Set-LocationLink",
-    "Test-Administrator",
-    "Repair-Emulators",
-    "Repair-Nexus7",
-    "Repair-Pixel"
+    "Test-Administrator"
 )
