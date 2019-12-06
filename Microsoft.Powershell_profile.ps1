6039e08adc0a11fc7cdd02b467e51ae2d8fa2590
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-12-06 14:47:30 -0600

    Import ImageTools module

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -6,7 +6,4 @@ Import-Module Utilities
 Import-Module MyPowershellGit
 Import-Module WebServer
 Import-Module VideoTools
-
-If (Test-Administrator) {
-    $Host.UI.RawUI.ForegroundColor = "Red"
-}
+Import-Module ImageTools
